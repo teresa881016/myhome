fd19d43a603f39f5bc0697d9438fd7acf2e5cf67
choisookyoung <choisk@8pin.co.kr>
2024-03-18 16:11:03 +0900

    링크 연결하기

---
+++ src/main/java/org/example/myhome/controller/HomeController.java
@@ -0,0 +1,12 @@
+package org.example.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
