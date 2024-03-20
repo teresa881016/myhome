c3f93a19cc8f1a9db133cca581ce6a759bab8581
choisookyoung <choisk@8pin.co.kr>
2024-03-20 09:42:46 +0900

    스프링 부트 - 게시판 만들기 PART 2

--- src/main/java/org/example/myhome/model/Board.java
+++ src/main/java/org/example/myhome/model/Board.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+
 
 @Entity
 @Data
@@ -13,6 +17,8 @@ class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하 입니다.")
     private String title;
     private String content;
 }
